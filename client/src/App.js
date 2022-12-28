68db3c60d88a99ae3825b8eb5729a30d54c5e2a4
Jacob Hocker <jacobdouglas06@gmail.com>
2022-12-28 16:03:55 -0600

    file structure update

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function App() {
+  const [listOfQuizzes, setListOfQuizzes] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:2000/quizzes").then((response) => {
+      setListOfQuizzes(response.data)
+    })
+  }, [])
+
 
   return (
     <div className='App'>
-      <h1>Hello World</h1>
+      {listOfQuizzes.map((val, key) => (
+        <div key={val.id}>
+          <h1>{val.quizName}</h1>
+        </div>
+      ))}
     </div>
   )
 }
