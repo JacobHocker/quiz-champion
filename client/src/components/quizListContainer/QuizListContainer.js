184fb6f5c06032e8015c3a84714b3e738322e6dd
Jacob Hocker <jacobdouglas06@gmail.com>
2023-01-23 09:54:49 -0600

    added some styling to quiz quiz card list

--- client/src/components/quizListContainer/QuizListContainer.js
+++ client/src/components/quizListContainer/QuizListContainer.js
@@ -5,25 +5,42 @@ import axios from 'axios';
 
 export default function QuizListContainer() {
     const [quizListDisplay, setQuizListDisplay] = useState([]);
-
+    
     let navigate = useNavigate();
     useEffect(() => {
         axios.get("http://localhost:2000/quizzes").then((response) => {
             setQuizListDisplay(response)
         })
     }, [])
+    
     return (
-        <div className='quiz-list-container'>
-            {quizListDisplay.data && quizListDisplay.data.map((val) => (
-                <div className='quiz-list-card' key={val.id}
-                onClick={() => {navigate(`/quiz/${val.id}`)}}>
-                    <h1 className='quiz-card-name'>{val.quizName}</h1>
-                    <h1>Quiz ID #: {val.id}</h1>
-                    <img src={val.quizImage} alt={val.quizName} className='quiz-card-thumb'/>
-                    <h3 className='quiz-card-cat'>{val.quizDifficulty}</h3>
-                    <p className='quiz-card-desc'>{val.quizDescription}</p>
-                </div>
-            ))}
+        <div className='quiz-list-page'>
+            <div className='quiz-list-header'>
+                <h1>Select a quiz to begin testing your knowledge!</h1>
+            </div>
+            <div className='quiz-list-container'>
+                {quizListDisplay.data && quizListDisplay.data.map((val) => (
+                    <div className='quiz-list-card' key={val.id}
+                    onClick={() => {navigate(`/quiz/${val.id}`)}}>
+
+                        <div className='quiz-card-header'>
+                            <h1>{val.quizName}</h1>
+                        </div>
+                        <div className='quiz-thumbnail-container'>
+                            <img src={val.quizImage} alt={val.quizName} className='quiz-card-thumb' />
+                        </div>
+                        <div className='quiz-card-body'>
+                            <h2>Difficulty: {val.quizDifficulty}</h2>
+                        </div>
+                        
+                        <div className='quiz-card-footer'>
+                            <p>{val.quizDescription}</p>
+                            <p>Quiz ID #: {val.id}</p>
+                        </div>
+                        
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
